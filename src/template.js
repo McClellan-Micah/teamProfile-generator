1ed31ed6da85b4127c319ca8e495eb5ce26df195
McClellan-Micah <mcclellan.micah.6@gmail.com>
2020-08-23 08:54:41 -0700

    create generateEngineer and generateIntern

--- src/template.js
+++ src/template.js
@@ -18,7 +18,45 @@ const generateTeam = (team) => {
         `;
   };
 
-  //follow generateManager outline for generateEngineer and generateIntern
+  //create engineer html
+  const generateEngineer = (engineer) => {
+    return `
+        <div class="card employee-card">
+            <div class="card-header">
+                <h2 class="card-title">${engineer.getName()}</h2>
+                <h3 class="card-title><i class="fas fa-mug-hot mr-2></i>${engineer.getRole()}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${engineer.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}}">${engineer.getEmail()}</a></li>
+                    <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+                </ul>
+            </div>
+        </div>
+      `;
+  };
+
+  //create intern html
+  const generateIntern = (intern) => {
+    return `
+        <div class="card employee-card">
+            <div class="card-header">
+                <h2 class="card-title">${intern.getName()}</h2>
+                <h3 class="card-title><i class="fas fa-mug-hot mr-2></i>${intern.getRole()}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${intern.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}}">${intern.getEmail()}</a></li>
+                    <li class="list-group-item">School: ${intern.getSchool()}</li>
+                </ul>
+            </div>
+        </div>
+      `;
+  };
+
+  //push html
 };
 
 //export function to generate entire page
