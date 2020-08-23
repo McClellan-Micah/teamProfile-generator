051775d94a4b9018737389cdec4419892af1b22d
McClellan-Micah <mcclellan.micah.6@gmail.com>
2020-08-23 10:39:50 -0700

    fix OUTPUT_DIR

--- index.js
+++ index.js
@@ -6,15 +6,13 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(_dirname, "output");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/template");
 
 const teamMembers = [];
 const idArray = [];
-
-console.log("Please build your team");
 
 //Retrieves manager name, employee ID, email address, and office number
 function appMenu() {
