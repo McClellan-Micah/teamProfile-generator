bf56dd3cf8a7c8dba1436119ac07ebff49a78074
McClellan-Micah <mcclellan.micah.6@gmail.com>
2020-08-23 09:02:29 -0700

    push html

--- src/template.js
+++ src/template.js
@@ -56,7 +56,27 @@ const generateTeam = (team) => {
       `;
   };
 
-  //push html
+  const html = [];
+
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Manager")
+      .map((manager) => generateManager(manager))
+  );
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Engineer")
+      .map((engineer) => generateEngineer(engineer))
+      .join("")
+  );
+  html.push(
+    team
+      .filter((intern) => intern.getRole() === "Intern")
+      .map((intern) => generateIntern(intern))
+      .join("")
+  );
+
+  return html.join("");
 };
 
 //export function to generate entire page
@@ -69,16 +89,19 @@ module.exports = (team) => {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Portfolio Demo</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-        <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+        <title>My Team</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
+        <script src="https://kit.fontawesome.com/c502137733.js"></script>   
     </head>
 
     <body>
         <header>
-        <div class="container flex-row justify-space-between align-center py-3">
-            <h1 class="page-title text-secondary py-2 px-3">My Team</h1>
+        <div class="row">
+            <div class="container flex-row justify-space-between align-center py-3">
+                <h1 class="page-title text-secondary py-2 px-3">My Team</h1>
+            </div>
         </div>
         </header>
         <main class="container my-5">
